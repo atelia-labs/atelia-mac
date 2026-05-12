56df67cadd7995bce0e0cde5140df20ffde73735
pdhaku0 <pdhaku0@gmial.com>
2026-05-12 20:48:31 +0900

    Simplify Mac baseline feature model

--- Sources/AteliaMacCore/MacClientFeature.swift
+++ Sources/AteliaMacCore/MacClientFeature.swift
@@ -2,47 +2,17 @@ import AteliaKit
 import Foundation
 
 public struct MacClientFeature: Sendable, Equatable, Hashable, Identifiable {
-    public var id: String
-    public var title: String
-    private var storedInitialScope: Bool
-
-    @available(*, deprecated, message: "MacClientFeature.initial contains baseline features only; future package scope is no longer represented here.")
-    public var isInitialScope: Bool {
-        get { storedInitialScope }
-        set { storedInitialScope = newValue }
-    }
+    public let id: String
+    public let title: String
 
     /// Creates a baseline Mac client feature.
     public init(id: String, title: String) {
         self.id = id
         self.title = title
-        self.storedInitialScope = true
-    }
-
-    /// Compatibility initializer for the previous mixed initial/future scope model.
-    /// The `isInitialScope` argument is retained only for source compatibility;
-    /// new baseline features should use `init(id:title:)`.
-    @available(*, deprecated, message: "Use init(id:title:). MacClientFeature.initial contains baseline features only.")
-    public init(id: String, title: String, isInitialScope: Bool) {
-        self.id = id
-        self.title = title
-        self.storedInitialScope = isInitialScope
     }
 
     public func sameIdentity(as other: MacClientFeature) -> Bool {
         id == other.id
-    }
-
-    public static func == (lhs: MacClientFeature, rhs: MacClientFeature) -> Bool {
-        lhs.id == rhs.id
-            && lhs.title == rhs.title
-            && lhs.storedInitialScope == rhs.storedInitialScope
-    }
-
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-        hasher.combine(title)
-        hasher.combine(storedInitialScope)
     }
 
     public static let initial: [MacClientFeature] = [
